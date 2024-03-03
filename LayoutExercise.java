2ddd60c3cdffce9203837199e36e7f57ada7db96
Charles Darwin Hudar <144643288+Charuzuuuu@users.noreply.github.com>
2024-03-03 10:00:17 +0800

    Create LayoutExercise.java

---
+++ LayoutExercise.java
@@ -0,0 +1,14 @@
+package com.example.projectcollection;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LayoutExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_exercise);
+    }
+}
